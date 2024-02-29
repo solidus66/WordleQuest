aafe34022a4ae38eb3f003fc53bd5e73e20a9419
solidus66 <66solidus@gmail.com>
2024-02-29 16:31:50 +0300

    added new form

---
+++ WordleQuest/EasterEgg.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace WordleQuest
+{
+    partial class EasterEgg
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(EasterEgg));
+            pictureBox1 = new PictureBox();
+            closeButton = new Button();
+            panel1 = new Panel();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(20, 20);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(300, 300);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // closeButton
+            // 
+            closeButton.BackColor = Color.FromArgb(225, 225, 225);
+            closeButton.Font = new Font("Segoe UI", 8F, FontStyle.Regular, GraphicsUnit.Point);
+            closeButton.Location = new Point(232, 8);
+            closeButton.Name = "closeButton";
+            closeButton.Size = new Size(104, 40);
+            closeButton.TabIndex = 1;
+            closeButton.Text = "ОК";
+            closeButton.UseVisualStyleBackColor = false;
+            closeButton.Click += closeButton_Click;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.Control;
+            panel1.Controls.Add(closeButton);
+            panel1.Location = new Point(0, 328);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(344, 64);
+            panel1.TabIndex = 2;
+            // 
+            // EasterEgg
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(342, 383);
+            Controls.Add(panel1);
+            Controls.Add(pictureBox1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "EasterEgg";
+            ShowIcon = false;
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "EasterEgg <3";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Button closeButton;
+        private Panel panel1;
+    }
+}
---
+++ WordleQuest/EasterEgg.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WordleQuest
+{
+    public partial class EasterEgg : Form
+    {
+        private string EasterEggImage;
+        public EasterEgg(string easterEggImage)
+        {
+            InitializeComponent();
+            EasterEggImage = easterEggImage;
+            pictureBox1.Image = Image.FromFile(EasterEggImage);
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
